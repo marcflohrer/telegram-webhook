06d5aee0ac822ebbec208f1398bcfc88353a5451
Marc Lohrer <Marc.Lohrer@gmx.de>
2024-09-08 09:08:15 +0200

    add telegram webhook

--- Telegram_BotNotify.cs
+++ Telegram_BotNotify.cs
@@ -1,24 +1,36 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
-namespace telegram_webhook
+public class Telegram_BotNotify
 {
-    public class Telegram_BotNotify
+    private readonly ILogger _logger;
+    private readonly TelegramBotClient _botClient;
+
+     public Telegram_BotNotify(ILogger<Telegram_BotNotify> logger, IConfiguration configuration)
     {
-        private readonly ILogger<Telegram_BotNotify> _logger;
+        _logger = logger;
+        var botToken = configuration["TELEGRAM_BOT_SECRET"];
+        _botClient = new TelegramBotClient(botToken);
+    }
 
-        public Telegram_BotNotify(ILogger<Telegram_BotNotify> logger)
-        {
-            _logger = logger;
-        }
+    [Function("Telegram_BotNotify")]
+    public async Task Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        [Function("Telegram_BotNotify")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        Update update = Newtonsoft.Json.JsonConvert.DeserializeObject<Update>(requestBody);
+
+        if (update?.Message != null)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            await _botClient.SendTextMessageAsync(
+                chatId: update.Message.Chat.Id,
+                text: $"Received your message {update.Message.Chat.Id}"
+            );
         }
     }
 }
